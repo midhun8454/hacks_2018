75b2866a8a35965789c05f28f2464131935426d5
Midhun K Nair <a-7876@LE1039.local>
2018-07-09 16:42:30 +0530

    Commit

---
+++ app/app.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.
+  module('phonecatApp').
+  config(['$locationProvider' ,'$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/phones', {
+          template: '<phone-list></phone-list>'
+        }).
+        when('/main', {
+          templateUrl: './views/main.html'
+        }).
+        otherwise('/phones');
+    }
+  ]);
---
+++ app/controller/mainController.js
@@ -0,0 +1,5 @@
+/*var app = angular.module("phonecatApp", []); 
+app.controller("mainController", function($scope) {
+    $scope.product = "midhun";
+    alert($scope.product)
+});*/
